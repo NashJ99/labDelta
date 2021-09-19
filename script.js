c4fc07ab9d8ea8bd3156782f776054682103dc3a
NashJ99 <89221371+NashJ99@users.noreply.github.com>
2021-09-19 14:14:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+$(function() {
+    console.log("document is ready!");
+
+    var userName, nameLength, nameMsg, nameReverse = ""; 
+
+    $('#btnName').click(function() {
+        userName = $('#userName').val(); 
+        // console.log(userName); 
+        // console.log($('#userName').val().length); 
+        // console.log(getStringLength(userName)); 
+
+        nameLength = getStringLength(userName);
+        nameMsg = "The Length of your name is: ";
+        $('#nameOutput').append(nameMsg + nameLength + "<br/>"); 
+
+        // console.log(reverseString(userName)); 
+
+        nameReverse = reverseString(userName);
+        nameMsg = "insert eldritch nusic hear ";
+        $('#nameOutput').append(nameMsg + nameReverse + "<br/>"); 
+
+        console.log(thirdLetter(userName)); 
+
+        stringArrayPosition(userName); 
+
+        });
+
+        $('#btnClear').click(function(){
+            clearElement("#nameOutput", "html");
+            clearElement("#userName", "input");
+      })
+
+    function getStringLength(stringVal){
+        var stringLength = stringVal.length; 
+       return stringLength; 
+    }
+
+    function reverseString(stringVal){
+        var revString = stringVal.split("").reverse().join("");
+       return revString; 
+    }
+
+    function thirdLetter(stringVal){
+        
+       return thirdString; var thirdString = stringVal[2]; 
+    }
+
+    function clearElement(elementId, elementType){
+        if(elementType == "html"){
+            $(elementId).html(""); 
+        }
+        else if(elementType=="input"){
+            $(elementId).val(" "); 
+        }
+      
+        
+    } 
+
+    function stringArrayPosition(stringVal){
+
+        for(i=0; i<stringVal.length; i++){
+            console.log(i + " " + stringVal[i]);
+        }
+        if (StringVal =13){
+            document.getElementById('namelength').innerHTML="unlucky name";
+        }
+        else{
+            document.getElementById('namelength').innerHTML="lucky name";
+
+        }
+        }
+
+
+
+    
+
+});
